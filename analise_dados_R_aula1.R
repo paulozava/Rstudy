ac646d3c9efdcc3a1a0024f49a3977a30313eedf
paulozava <camposzava@gmail.com>
2018-07-10 12:41:40 +0200

    utilizando funções inbutidas

--- analise_dados_R_aula1.R
+++ analise_dados_R_aula1.R
@@ -1,8 +1,19 @@
 library(readxl)
 aulas <- read_excel("aulas.xlsx")
-View(aulas)
+attach(aulas)
+# View(aulas)
 # legendas do arquivo
 # user_id = usuário do curso
 # course_id = curso realizado
 # section_id = video específico do curso
 
+sort(section_id)
+head(section_id)
+
+#corrigindo umo outlier
+aulas[33137, 3] <- 3255
+attach(aulas)
+tail(section_id)
+
+View(unique(sort(section_id)))
+length(unique(sort(section_id)))
