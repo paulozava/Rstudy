533959b5f65fb7bca842257e2e4f8c87e15f51df
paulozava <camposzava@gmail.com>
2018-07-10 15:50:08 +0200

    tratamento do dataframe popularidade e duração

--- Analise de Dados com R/aula04.R
+++ Analise de Dados com R/aula04.R
@@ -6,13 +6,19 @@ duracao <- read_csv('duracao.csv')
 
 library(plyr)
 duracao <- rename(duracao, replace=c('user_id'='aluno', 'course_id'='curso', 'timeToFinish' = 'dias'))
+populariade <- rename(populariade, replace=c('course_id'='curso', 'freq'='frequencia'))
+head(populariade)
 
 sumario_media <- aggregate(duracao$dias, list(duracao$curso), mean, na.rm=TRUE)
 sumario_mediana <- aggregate(duracao$dias, list(duracao$curso), median, na.rm=TRUE)
+head(sumario_media)
 
-sumario_media <- rename(sumario_media, replace=c('Group.1'='curso', 'x'='media'))
-sumario_mediana <- rename(sumario_mediana, replace=c('Group.1'='curso', 'x'='mediana'))
-head(sumario_mediana)
+sumario <- merge(sumario_media, sumario_mediana, by = 'Group.1')
+head(sumario)
+sumario <- rename(sumario, replace=c('Group.1'='curso', 'x.x'='media', 'x.y'='mediana'))
+head(sumario)
 
-popularidade_duracao <- merge(sumario_media, populariade, by = intersect(names(sumario_media), names(populariade)), all = FALSE)
+popularidade_duracao <- merge(sumario, populariade, by = 'curso')
 head(popularidade_duracao)
+
+popularidade_duracao$X1 <- NULL
