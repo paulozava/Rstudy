484760971dc98275a2506a9b8741d3530d8783cc
paulozava <camposzava@gmail.com>
2018-07-10 14:09:24 +0200

    verificando plot

--- Analise de Dados com R/aula02.R
+++ Analise de Dados com R/aula02.R
@@ -9,3 +9,13 @@ library(plyr)
 duracao <- rename(duracao, replace=c('user_id'='aluno', 'course_id'='curso', 'timeToFinish' = 'dias'))
 View(duracao)
 
+plot(duracao$dias)
+mean(duracao$dias)
+
+hist(duracao$dias)
+h <- hist(duracao$dias, breaks = 1000)
+plot(h, freq=True)
+
+h<-hist(duracao$dias, plot=F, breaks = 1000)
+h$counts <- (h$counts / sum(h$counts))*100
+plot(h, freq=TRUE, ylab="Relative Frequency")
