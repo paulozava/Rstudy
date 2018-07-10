072b1fa1d716e9ba2f205f4771bfb281c79d67fe
paulozava <camposzava@gmail.com>
2018-07-10 16:52:46 +0200

    terminando a plotagem

--- Analise de Dados com R/aula05.R
+++ Analise de Dados com R/aula05.R
@@ -1,3 +1,15 @@
 # importar dados da aula04
 
 plot(popularidade_duracao$frequencia, popularidade_duracao$mediana)
+modelo.linear <- lm(popularidade_duracao$mediana ~ popularidade_duracao$frequencia)
+abline(modelo.linear, col='red')
+summary(modelo.linear)
+
+install.packages("ggplot2")
+library("ggplot2")
+
+grafico <- ggplot(popularidade_duracao, aes(frequencia, media)) +
+        geom_point() +
+        geom_smooth()
+
+grafico
