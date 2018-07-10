6166a43d39827d93e3aa60daf1db3558bf8a5322
paulozava <camposzava@gmail.com>
2018-07-10 14:58:36 +0200

    merge

--- Analise de Dados com R/aula02.R
+++ Analise de Dados com R/aula02.R
@@ -13,9 +13,13 @@ plot(duracao$dias)
 mean(duracao$dias)
 
 hist(duracao$dias)
-h <- hist(duracao$dias, breaks = 1000)
-plot(h, freq=True)
+hist(duracao$dias, breaks = 1000)
 
 h<-hist(duracao$dias, plot=F, breaks = 1000)
 h$counts <- (h$counts / sum(h$counts))*100
 plot(h, freq=TRUE, ylab="Relative Frequency")
+
+mean(duracao$dias, na.rm=TRUE)
+median(duracao$dias, na.rm=T)
+summary(duracao$dias)
+
