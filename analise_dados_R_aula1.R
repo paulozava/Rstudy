7ea1e5a2726102921e1a518ecaf3bd5ec341a157
paulozava <camposzava@gmail.com>
2018-07-10 11:12:59 +0200

    import data from a excel file

---
+++ analise_dados_R_aula1.R
@@ -0,0 +1,8 @@
+library(readxl)
+aulas <- read_excel("aulas.xlsx")
+View(aulas)
+# legendas do arquivo
+# user_id = usuário do curso
+# course_id = curso realizado
+# section_id = video específico do curso
+
